3e95b2474532b92c23438120cc3e07372cc3ac8f
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-07-04 16:31:22 +0200

    fixed something

--- src/server/PlayerExperience.js
+++ src/server/PlayerExperience.js
@@ -5,8 +5,7 @@ export default class PlayerExperience extends Experience {
   constructor(clientType) {
     super(clientType);
 
-    this.checkin = this.require('checkin');
-    this.sharedConfig = this.require('shared-config');
+    this.placer = this.require('placer');
   }
 
   // if anything needs to append when the experience starts
